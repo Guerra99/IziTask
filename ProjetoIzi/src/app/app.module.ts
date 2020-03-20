9d2f6bc2d685b26f9f04fd9b86340620ba8f707c
Guilherme Guerra <guilherme.guerra9@hotmail.com>
2020-03-20 17:13:02 -0300

    landing

--- ProjetoIzi/src/app/app.module.ts
+++ ProjetoIzi/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './containers/landing-page/landing-page.component';
+import { LoginComponent } from './containers/login/login.component';
+import { CadastroUsuarioComponent } from './containers/cadastro-usuario/cadastro-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginComponent,
+    CadastroUsuarioComponent
   ],
   imports: [
     BrowserModule,
