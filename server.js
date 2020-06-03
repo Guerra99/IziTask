b869c7fbca82c90f4218fb5e0f2f0abb78342ee4
Guilherme Guerra <guilherme.guerra9@hotmail.com>
2020-06-03 14:53:57 -0300

    ajustes deploy

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+
 const app = express();
 
-app.use(express.static('Izi/src/index.html'));
+app.use(express.static(__dirname + '/dist/Izi'));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join('Izi/src/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/Izi/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
