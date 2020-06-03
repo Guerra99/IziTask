aaa1690d4fd2138583cac8556dee34343b8caf2c
Guilherme Guerra <guilherme.guerra9@hotmail.com>
2020-06-03 19:27:05 -0300

    ajustes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/TCC/Izi'));
+app.use(express.static(__dirname + 'server.js'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/TCC/Izi/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
